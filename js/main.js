1c43264079da9c865f56f5a9f26ccc221cae8dd4
PVmendez <valentino.mendez.rey@gmail.com>
2022-06-13 10:57:27 -0300

    Slider

--- js/main.js
+++ js/main.js
@@ -189,3 +189,33 @@ jQuery(document).ready(function(){
   function myFunction(x) {
     x.classList.toggle("change");
   }
+
+  
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 50) {
+            $(".navbar").addClass("sticky-nav");
+        } else {
+            $(".navbar").removeClass("sticky-nav");
+        }
+    });
+
+    // BACKSTRETCH SLIDESHOW
+    $('#section_1').backstretch([
+      "img/microsoft-edge-FAaz8lkinzs-unsplash.jpg", 
+      "img/surface-1x5jnhtlp3Y-unsplash.jpg",
+      "img/surface-71_s6RDJpGc-unsplash.jpg"
+    ],  {duration: 2000, fade: 750});
+    
+  })(window.jQuery);
+
