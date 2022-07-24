5daf3cd38e0f67ceaf9d018e59a957e69ab0ae52
PVmendez <valentino.mendez.rey@gmail.com>
2022-07-24 10:56:09 -0300

    Update 9.0

---
+++ js/music-list.js
@@ -0,0 +1,49 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Podcast 1",
+    artist: "Natalie Cederbaum",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Podcast 2",
+    artist: "Natalie Cederbaum",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Podcast 3",
+    artist: "Natalie Cederbaum",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Podcast 4",
+    artist: "Natalie Cederbaum",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Podcast 5",
+    artist: "Natalie Cederbaum",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Podcast 6",
+    artist: "Natalie Cederbaum",
+    img: "music-6",
+    src: "music-6"
+  },
+];
