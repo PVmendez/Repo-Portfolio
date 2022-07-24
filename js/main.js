1dbe5841f24ff3e17937f32bddb5d2a00351fc93
PVmendez <valentino.mendez.rey@gmail.com>
2022-07-24 07:30:44 -0300

    Update 8.0

--- js/main.js
+++ js/main.js
@@ -212,10 +212,27 @@ jQuery(document).ready(function(){
 
     // BACKSTRETCH SLIDESHOW
     $('#section_1').backstretch([
+      "img/WEB/inerte.jpg",
       "img/WEB/ANTIFASHION-657.jpg", 
-      "img/WEB/ANTIFASHION-834.jpg",
-      "img/WEB/PORTADA ANTIFASHION_compressed_page-0001.jpg"
+      "img/WEB/inerte2.jpg"
     ],  {duration: 2000, fade: 750});
     
   })(window.jQuery);
+
+
+  var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
 
